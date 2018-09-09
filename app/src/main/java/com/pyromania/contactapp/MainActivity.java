90d206009f50486e32210aaec795681aa891e684
New <naj690@yahoo.com>
2018-09-10 00:35:28 +0800

    Initial commit

---
+++ app/src/main/java/com/pyromania/contactapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.pyromania.contactapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvContactTitle;
+    Button btnAdd;
+    ImageView ivType, ivCall, ivWeb, ivLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvContactTitle = findViewById(R.id.tvContactTitle);
+        btnAdd = findViewById(R.id.btnAdd);
+        ivType = findViewById(R.id.ivType);
+        ivCall = findViewById(R.id.ivCall);
+        ivWeb = findViewById(R.id.ivWeb);
+        ivLocation = findViewById(R.id.ivLocation);
+
+        // set images to be invisible upon load
+        ivType.setVisibility(View.GONE);
+        ivCall.setVisibility(View.GONE);
+        ivWeb.setVisibility(View.GONE);
+        ivLocation.setVisibility(View.GONE);
+
+        // selecting "add contact" and moving to Input Activity
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.pyromania.contactapp.Input.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
